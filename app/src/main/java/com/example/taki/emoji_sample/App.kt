f62cb4452121ca7238436cd22765bd7128f907cb
taki4227 <takiki.truth@gmail.com>
2020-03-18 18:57:15 +0900

    Add debug code

--- app/src/main/java/com/example/taki/emoji_sample/App.kt
+++ app/src/main/java/com/example/taki/emoji_sample/App.kt
@@ -1,6 +1,7 @@
 package com.example.taki.emoji_sample
 
 import android.app.Application
+import android.graphics.Color
 import android.util.Log
 import androidx.core.provider.FontRequest
 import androidx.emoji.text.EmojiCompat
@@ -22,6 +23,11 @@ class App : Application() {
         )
         val config = FontRequestEmojiCompatConfig(applicationContext, fontRequest)
             .setReplaceAll(true)
+            // If emoji is replaced by EmojiCompat, change its background color.
+            // For debug
+            .setEmojiSpanIndicatorEnabled(true)
+            .setEmojiSpanIndicatorColor(Color.GREEN)
+            // EmojiCompat initialize listener
             .registerInitCallback(object : EmojiCompat.InitCallback() {
                 override fun onInitialized() {
                     Log.i("smash", "EmojiCompat initialized")
